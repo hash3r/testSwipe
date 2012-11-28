5e147cedf824efe9babe1f370177a2fdd23cd865
hash3r <vovagn@ya.ru>
2012-11-28 23:16:06 +0200

    Initial Commit

---
+++ testSwipe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testSwipe
+//
+//  Created by Andrew on 28.11.12.
+//  Copyright (c) 2012 Andrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
